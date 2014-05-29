b1d41a0604bd25de8ed279de5f946c30cbb3c146
Gregory Jefferis <jefferis@gmail.com>
2014-05-30 00:09:10 +0100

    sty: typos and messages

--- 03-helmstaedter2013/01-download.R
+++ 03-helmstaedter2013/01-download.R
@@ -1,4 +1,4 @@
-# There is presently a problem reading the main matlad skeleton dataset into R
+# There is presently a problem reading the main Matlab skeleton dataset into R
 # because it is uses advanced MAT format features that are not supported by
 # the R.matlab package. Therefore I have provided a canned download for the time
 # being
@@ -6,10 +6,11 @@
 urls=file.path("http://flybrain.mrc-lmb.cam.ac.uk/si/nat/helmstaedter/",
   c('sk.uniq.rda','skeleton_metadata.rda'))
 
-message("Downloading data (30Mb) if necessary ...")
+message("Checking for presence of data (30Mb) if necessary ...")
 for (url in urls){
   localfile=basename(url)
   if(file.exists(localfile)) next
+  message("Downloading data ...")
   t=try(download.file(url,localfile))
   if(inherits(t,'try-error')) {
     message("unable to download ", url)
