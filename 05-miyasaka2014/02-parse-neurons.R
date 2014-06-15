201e07e68c63411dfa85013b78d49d24775bb6e6
James Manton <ajd.manton@googlemail.com>
2014-06-15 21:39:04 +0100

    Fix bug and add saving of neurons

--- 05-miyasaka2014/02-parse-neurons.R
+++ 05-miyasaka2014/02-parse-neurons.R
@@ -5,8 +5,8 @@ if(!length(swcs)) stop("No traces found! See 01-download-zips.R!")
 
 df=data.frame(name=sub("\\.swc","",basename(swcs)),
   gene=factor("lhx2a", levels=c("lhx2a", "tbx21")),
-  cluster=factor(sub("([a-zA-Z]+)-.*","\\1",names(zm))),
   stringsAsFactors=FALSE)
+df$cluster=factor(sub("([a-zA-Z]+)-.*","\\1",df$name))
 df$gene[grep("-T",df$name)]="tbx21"
 rownames(df)=df$name
 zm=read.neurons("pruned-swc", neuronnames = df$name, df=df)
@@ -20,3 +20,6 @@ plot3d(zm, cluster=='mdG', col=gene, lwd=2, soma=T)
 # plot all lhx2a neurons coloured by cluster
 clear3d()
 plot3d(zm, gene=='lhx2a', col=cluster, lwd=2, soma=T)
+
+# Save all neurons
+saveRDS(zm, file='neurons.rds')
