3823865f0e233aeaa852e44444fc4ab05da6b576
Gregory Jefferis <jefferis@gmail.com>
2014-06-17 17:24:31 +0100

    fix: need to find x position of neuron start points

--- 05-miyasaka2014/03-mirror-neurons.R
+++ 05-miyasaka2014/03-mirror-neurons.R
@@ -2,6 +2,7 @@ zm <- readRDS('neurons.rds')
 
 # Mirror all neurons past middle of x range
 mid_x <- 248.857/2
+root_xs<-sapply(zm,function(x) x$d$X[x$StartPoint])
 neurons_to_mirror <- names(root_xs[root_xs > mid_x])
 zm.canonical <- zm
 zm.canonical[neurons_to_mirror] <- mirror(zm[neurons_to_mirror], warpfile="mirroring_registration.list/", mirrorAxisSize=248.857)
