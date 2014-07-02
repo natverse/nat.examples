b93041dde12b1433177817a1a0bc450535019de2
Gregory Jefferis <jefferis@gmail.com>
2014-07-02 14:20:57 +0100

    just add zenodo url to ones to ge

--- 04-sumbul2014/01-download-zips.R
+++ 04-sumbul2014/01-download-zips.R
@@ -3,17 +3,14 @@
 
 message("Downloading supplementary data from Nature Comms website (~70Mb)")
 urls=sprintf('http://www.nature.com/ncomms/2014/140324/ncomms4512/extref/ncomms4512-s%d.zip',c(2,13,14))
+# alternative download location for third file (s14.zip->S13.mat)
+urls=c(urls,'http://zenodo.org/record/10737/files/ncomms4512-s14.zip')
 for (url in urls){
 	localfile=basename(url)
 	if(file.exists(localfile)) next
 	t=try(download.file(url,localfile))
 	if(inherits(t,'try-error')) {
 		message("unable to download ", url)
-		if(grepl("14\\.zip",url)){
-			# fetch from alternative location
-			url='http://zenodo.org/record/10737/files/ncomms4512-s14.zip'
-			download.file(url,localfile)
-		}
 		next
 	}
 	unzip(localfile)
