fcbf8d961ec83c775b1fe1ff77f5005f775e483c
Gregory Jefferis <jefferis@gmail.com>
2014-09-27 22:10:19 +0100

    first example plots for Sumbul

---
+++ 04-sumbul2014/03-plot-neurons.R
@@ -0,0 +1,18 @@
+library(nat)
+# load in cached version of processed neurons from disk
+load("sumbuln.rda")
+
+# plot everybody except GFP neurons
+# start to see some clear stratification
+plot3d(subset(sumbuln, geneticLine!="GFP-YFP"), col=rainbow(nlevels(geneticLine))[geneticLine])
+
+# the with.neuronlist method gives you convenient access to the attached metadata frame
+# here we summarise the "cluster" letter for each neuron
+with(sumbuln, summary(cluster))
+# ... and the genetic line
+with(sumbuln, summary(geneticLine))
+# here we plot the cell body positions as spheres
+# coloured by the geneticLine used
+with(sumbuln,
+  spheres3d(X,Y,Z, radius=4, col=rainbow(nlevels(geneticLine))[geneticLine])
+)
