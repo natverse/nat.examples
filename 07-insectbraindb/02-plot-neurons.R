b1adb74ccd794f54264bc1ff2b8533147ce67080
Gregory Jefferis <jefferis@gmail.com>
2015-04-03 19:05:38 +0100

    fix typo in neuronlist

--- 07-insectbraindb/02-plot-neurons.R
+++ 07-insectbraindb/02-plot-neurons.R
@@ -3,11 +3,12 @@ library(nat)
 load("tedore_neurons.rda")
 
 # what kind of neurons do we have?
-head(tedore_neurons)
+head(tedoren)
 
 # the with.neuronlist method gives you convenient access to the attached metadata frame
 # here we summarise the "Species" for each neuron
 with(tedoren, summary(Species))
 
 # plot neurons from specific structural cluster
+nopen3d()
 plot3d(subset(tedoren, Species=="Danaus plexippus"))
