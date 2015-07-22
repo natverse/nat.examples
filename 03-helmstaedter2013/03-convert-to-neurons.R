1372e49e24736e38c7b8392cd208d61c534dda79
Gregory Jefferis <jefferis@gmail.com>
2015-07-22 16:13:04 +0200

    also include swc export of helmstaedter neurons

--- 03-helmstaedter2013/03-convert-to-neurons.R
+++ 03-helmstaedter2013/03-convert-to-neurons.R
@@ -4,4 +4,7 @@ load("sk.uniq.rda")
 
 library(nat)
 skn=nlapply(sk.uniq, as.neuron, OmitFailures = T, .progress='text')
+# save neurons in native R format
 save(skn, file='skn.rda')
+# save a zip archive of SWC format neurons for all reconstructions
+write.neurons(skn, dir='skuniq.swc.zip', files=names(skn), format='swc')
