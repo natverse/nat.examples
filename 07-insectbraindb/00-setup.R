fb07d94399f43949bbfbed20e546e944b28c158f
Gregory Jefferis <jefferis@gmail.com>
2015-09-15 21:20:45 +0100

    just dendextend
    * largely supersedes my own dendroextras

--- 07-insectbraindb/00-setup.R
+++ 07-insectbraindb/00-setup.R
@@ -2,7 +2,6 @@ if(!require('nat')) install.packages("nat")
 if(!require('rvest')) install.packages("rvest")
 if(!require('nat.nblast')) install.packages("nat.nblast")
 if(!require('dendextend')) install.packages("dendextend")
-if(!require('dendroextras')) install.packages("dendroextras")
 
 library(nat)
 library(rvest)
