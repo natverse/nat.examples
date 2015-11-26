0c4fa63a0fd754d4ad54caced5148740f0709992
Gregory Jefferis <jefferis@gmail.com>
2015-11-26 18:06:12 +0000

    wrap dir change in try
    * in case we are in a situtation where it can't work
      such as being sourced from knitr

--- 03-helmstaedter2013/00-setup.R
+++ 03-helmstaedter2013/00-setup.R
@@ -3,7 +3,7 @@ if(!require('nat')) install.packages("nat")
 library(nat)
 
 # set working directory to location of this file
-setwd(dirname(attr(body(function() {}),'srcfile')$filename))
+try(setwd(dirname(attr(body(function() {}),'srcfile')$filename)))
 
 #' plot3d method for Moritz's neurons
 #'
