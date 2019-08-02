4b95c9d7e07e7713b2984a96832ff5e4a188ebb1
alexanderbates <alexander.shakeel.bates@gmail.com>
2019-08-02 20:56:27 +0100

    Update 02-sample-plots.R

--- 03-helmstaedter2013/02-sample-plots.R
+++ 03-helmstaedter2013/02-sample-plots.R
@@ -1,8 +1,12 @@
+## This script assumed that you have run the file "03-helmstaedter2013/00-setup.R" and "03-helmstaedter2013/01-download.R"
+
+# Read in neurons
 load("sk.uniq.rda")
 load("skeleton_metadata.rda")
 
 # All neurons, coloured by raw type id
 plot3d(sk.uniq,col=factor(typeid))
+
 # add (uncoloured) somata
 spheres3d(t(t(skeleton_metadata$kn.e2006.ALLSKELETONS.FINAL2012.allSomata[,1:3])*c(16.5,16.5,25)),rad=2000)
 
@@ -22,7 +26,7 @@ plot3d(sk.uniq,class=='bipolar',col=factor(stypeid))
 # TODO Check "glial" identity
 clear3d();plot3d(sk.uniq,class=='glial',col='grey')
 
-
+# all together now
 clear3d()
 plot3d(sk.uniq,class=='bipolar',col='grey')
 plot3d(sk.uniq,ntype=='starburst amacrine',col=stypeid)
