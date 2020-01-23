0f60a7f91858944ddc564831179a5c4301b37408
alexanderbates <alexander.shakeel.bates@gmail.com>
2020-01-23 20:25:34 +0000

    Create 01-find-neurons.R

---
+++ 15-hemibrain/01-find-neurons.R
@@ -0,0 +1,9 @@
+# This script assumed that you have run the file "15-hemibrain/00-setup.R"
+
+## What do we want to do with all this hemibrain data?
+### Perhaps we could look at some popular neuron types and see if they connect to each other?
+### Mushroom body output neurons (MBONs) are a well-studied neuron class.
+### They read-out stored associative memories, 'located' in the mushroom body of the flies.
+### Let's find some in our HemiBrain data:
+
+
