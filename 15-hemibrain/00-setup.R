d1d7a67ab5f93908aca196c73b91ad34460dc1b5
alexanderbates <alexander.shakeel.bates@gmail.com>
2020-01-24 00:33:21 +0000

    Update 00-setup.R

--- 15-hemibrain/00-setup.R
+++ 15-hemibrain/00-setup.R
@@ -1,11 +1,31 @@
 # install
 if(!require('devtools')) install.packages("devtools")
 if(!require('natverse')) devtools::install_github("natverse/natverse")
+if(!require('ComplexHeatmap')) install.packages("ComplexHeatmap")
 
 # load
 library(natverse)
 library(neuprintr)
 library(dendroextras)
+library(ComplexHeatmap)
 
 # set working directory to location of this file
 try(setwd(dirname(attr(body(function() {}),'srcfile')$filename)))
+
+# Colours
+# some nice colors!! Inspired by LaCroixColoR
+lacroix = c("#C70E7B", "#FC6882", "#007BC3", "#54BCD1", "#EF7C12", "#F4B95A", 
+            "#009F3F", "#8FDA04", "#AF6125", "#F4E3C7", "#B25D91", "#EFC7E6", 
+            "#EF7C12", "#F4B95A", "#C23A4B", "#FBBB48", "#EFEF46", "#31D64D", 
+            "#132157","#EE4244", "#D72000", "#1BB6AF")
+names(lacroix) = c("purple", "pink",
+                   "blue", "cyan",
+                   "darkorange", "paleorange",
+                   "darkgreen", "green",
+                   "brown", "palebrown",
+                   "mauve", "lightpink",
+                   "orange", "midorange",
+                   "darkred", "darkyellow",
+                   "yellow", "palegreen", 
+                   "navy","cerise",
+                   "red", "marine")
