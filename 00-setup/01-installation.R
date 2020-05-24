0d773dc83337690f10134db5ee187c207dd9373a
Gregory Jefferis <jefferis@gmail.com>
2020-05-24 23:18:03 +0100

    install logic was backwards
    require returns TRUE if package is available
    FALSE if it is not available (and needs installing)

--- 00-setup/01-installation.R
+++ 00-setup/01-installation.R
@@ -1,7 +1,8 @@
 # Install the nat tools
-if(require(natmanager)){ install.packages("natmanager")}
+if(!require("natmanager")) install.packages("natmanager")
 # See http://natverse.org/install/ for details / troubleshooting
 natmanager::install("natverse")
-if(require(ggpubr)){install.packages("ggpubr")}
-if(require(reshape2)){install.packages("reshape2")}
-if(require(catnat)){remotes::install_github("jefferislab/catnat")}
+
+if(!require("ggpubr")) install.packages("ggpubr")
+if(!require("reshape2")) install.packages("reshape2")
+if(!require("catnat")) remotes::install_github("jefferislab/catnat")
