fa1fadeb1084bdb379397a94172cf68877649d3c
Gregory Jefferis <jefferis@gmail.com>
2020-05-24 23:46:28 +0100

    improved install

--- 01-basics/00-setup.R
+++ 01-basics/00-setup.R
@@ -1,10 +1,25 @@
 # install
-if(!require('devtools')) install.packages("devtools")
-if(!require('tidyverse')) install.packages("tidyverse")
-if(!require('natverse')) devtools::install_github("natverse/natverse")
-if(!require('lhns')) devtools::install_github("jefferislab/lhns")
+if(!require("natmanager")) install.packages("natmanager")
+# See http://natverse.org/install/ for details / troubleshooting
 
-# load natversee functions
+if(!require('natverse')) natmanager::install("natverse")
+
+# This is a big download. 
+if(!require('lhns')) devtools::install_github("jefferislab/lhns")
+# If you have trouble installing lhns with `install_github``
+
+## in the terminal. Choose a directory that you like
+# cd  ~/natverse
+## use --depth to download only the latest revision (1GB instead of 2B for whole history)
+# git clone --depth 1 https://github.com/jefferislab/lhns
+
+## This still took me serveral mins to download, but it is much less fragile that the install_github mechanism and you get some indication of progress
+
+## Then, in R (adjusting path to git checkout of lhns as appropriate)
+# remotes::install_local("~/natverse/lhns")
+
+
+# load natverse functions
 library(natverse)
 library(tidyverse)
 library(lhns)
@@ -12,6 +27,4 @@ library(lhns)
 ## It contains some data we will play with in the next R file
 
 # set working directory to location of this file
-try(setwd(dirname(attr(body(function() {}),'srcfile')$filename)))
-
-
+setwd(here::here('01-basics'))
