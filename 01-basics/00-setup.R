23218937f6a95f224abe8b59f65ea62aed8c86be
Gregory Jefferis <jefferis@gmail.com>
2020-05-25 16:19:57 +0100

    install_git works from inside R for lhns package
    * although probably takes 2x as long as git clone --depth 1

--- 01-basics/00-setup.R
+++ 01-basics/00-setup.R
@@ -4,20 +4,26 @@ if(!require("natmanager")) install.packages("natmanager")
 
 if(!require('natverse')) natmanager::install("natverse")
 
-# This is a big download. 
-if(!require('lhns')) devtools::install_github("jefferislab/lhns")
-# If you have trouble installing lhns with `install_github``
+# This is a big download. We use install_git because this is more reliable that
+# install_github for such a large repo (~2GB). It should take 5-30m depending on 
+# your internet connection, but note that there is no progress information.
+if(!require('lhns')) 
+  remotes::install_git("git://github.com/jefferislab/lhns.git")
 
+# Alternatively, if you want to see what is going on and don't mind using the
+# Terminal you can do:
+
+# --- manual install of lhns via command line git ---
 ## in the terminal. Choose a directory that you like
 # cd  ~/natverse
 ## use --depth to download only the latest revision (1GB instead of 2B for whole history)
 # git clone --depth 1 https://github.com/jefferislab/lhns
 
-## This still took me serveral mins to download, but it is much less fragile that the install_github mechanism and you get some indication of progress
-
+## This still took me serveral mins to download, but it is much less fragile 
+## that the install_github mechanism and you get some indication of progress
 ## Then, in R (adjusting path to git checkout of lhns as appropriate)
 # remotes::install_local("~/natverse/lhns")
-
+# --- end manual install of lhns via command line git ---
 
 # load natverse functions
 library(natverse)
