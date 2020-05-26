2b001a6914452013439a75dd744f027ad6936d68
Gregory Jefferis <jefferis@gmail.com>
2020-05-26 02:04:16 +0100

    Simplifying butterfly meshes / neurons
    * the full size ones are very slow when plotted with transparency

--- 07-insectbraindb/02-plot-neurons.R
+++ 07-insectbraindb/02-plot-neurons.R
@@ -18,8 +18,8 @@ available.brains
 ## Great, they do, let's get it
 butterfly.brain = insectbraindb_read_brain(species = "Danaus plexippus")
 
-## And plot in a translucent manner
-plot3d(butterfly.brain, alpha = 0.1)
+## And plot as a wireframe
+wire3d(butterfly.brain, col='gold')
 rgl.snapshot(file = paste0("images/MonarchButterly_ReppertLab_.png"), fmt = "png")
 
 ## Oop, that's a lot of neuropils. 
@@ -30,5 +30,28 @@ butterfly.brain$neuropil_full_names
 ## There lateral horn (LH) and the antennal lobe (AL) are my favourites.
 ## Let's plot those
 clear3d()
-plot3d(subset(butterfly.brain, "LH|AL"), alpha = 0.5)
 plot3d(butterfly.neurons, lwd = 2, soma = 5)
+plot3d(subset(butterfly.brain, "LH|AL"), alpha = 0.3)
+
+## Sometimes it's useful to make simplified versions of neurons / meshes
+
+# the neurons seem to be rather densely sampled
+steplengths=unlist(sapply(butterfly.neurons,seglengths, sumsegment=F))
+# (for details, ?seglengths)
+hist(steplengths, br=400, xlim=c(0,1), col='red')
+
+# Let's resample to 1µm which is probably fine for visualisation
+butterfly.neurons.rs1=resample(butterfly.neurons, stepsize = 1)
+nclear3d()
+plot3d(butterfly.neurons.rs1, lwd = 2, soma = 5)
+
+# Plotting all neuropils in one go with alpha is pretty slow.
+# Instead we can combine them into a single mesh
+wholebrain.hires=as.mesh3d(butterfly.brain)
+# subsample to 10% of number of nodes
+wholebrain=Rvcg::vcgQEdecim(wholebrain.hires, percent = 0.1)
+# add vertex normals for smoother looking plot
+wholebrain=addNormals(wholebrain)
+# now plot
+shade3d(wholebrain, col='gold', alpha=.1)
+
